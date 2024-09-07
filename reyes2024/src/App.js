4ffed06a2fa2d48a074f830b913215e0febca57e
Alyssa Reyes <104232225+2REYES1@users.noreply.github.com>
2024-09-07 13:10:16 +0000

    added GreetingPage component with slight css

--- reyes2024/src/App.js
+++ reyes2024/src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
 import bootstrap from 'bootstrap';
+import GreetingPage from './Components/GreetingPage/GreetingPage';
 
 
 function App() {
   return (
     <div className="App">
-      <p>hello</p>
+      <GreetingPage />
     </div>
   );
 }
