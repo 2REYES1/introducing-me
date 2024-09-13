715ff0963a5e35cc28be04c03f506c803e433a26
Alyssa Reyes <104232225+2REYES1@users.noreply.github.com>
2024-09-13 02:09:13 +0000

    AlyssaPage added. Does not function well

--- reyes2024/src/Components/GreetingPage/GreetingPage.js
+++ reyes2024/src/Components/GreetingPage/GreetingPage.js
@@ -1,20 +1,18 @@
 import './GreetingPage.css';
 import * as React from 'react';
 
-function GreetingPage(){
-  return(
-    <div class="container main-wrapper-gp" id="greeting-page">
-
-      <div class="row h-100 align-items-center justify-content-center">
-        <div class="flex-column-xs-12 justify-content-center text-center">
-          <h1 class="nanum-pen-gp">Hello, I'm Alyssa!</h1>
-          <p class="roboto-mono-gp">a uc irvine computer science student who loves the endless knowledge that comes with tech</p>
-          <button href="mailto:alyssalareyes@gmail.com" class="nanum-pen">Contact Me</button>
+function GreetingPage() {
+  return (
+    <div className="container main-wrapper-gp" id="greeting-page">
+      <div className="row h-100 align-items-center justify-content-center">
+        <div className="flex-column-xs-12 justify-content-center text-center">
+          <h1 className="nanum-pen-gp">Hello, I'm Alyssa!</h1>
+          <p className="roboto-mono-gp">a uc irvine computer science student who loves the endless knowledge that comes with tech</p>
+          <button href="mailto:alyssalareyes@gmail.com" className="nanum-pen-gp">Contact Me</button>
         </div>
-
       </div>
     </div>
-  )
+  );
 }
 
 export default GreetingPage;
