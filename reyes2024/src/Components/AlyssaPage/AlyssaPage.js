b1200809dbc7f03d65eeaf68e445ea5eddbcb3aa
Alyssa Reyes <alyssalareyes@gmail.com>
2024-09-13 18:55:25 -0700

    added a project showcase page but nothing on there. trying to fix spacing on alyssapage

--- reyes2024/src/Components/AlyssaPage/AlyssaPage.js
+++ reyes2024/src/Components/AlyssaPage/AlyssaPage.js
@@ -2,35 +2,44 @@ import './AlyssaPage.css';
 import React from 'react';
 import AlyssaPicture from './imgs/ally.JPG';
 
+
+
+
 function AlyssaPage() {
   return (
     <div className="container main-wrapper-ap" id="alyssa-page">
-      <div className="row h-100 align-items-center justify-content-center">
-        <div className="col-md-3 justify-content-center text-center m-5">
-          <h3 className="nanum-pen-ap">ALYSSA REYES</h3>
-          <img src={AlyssaPicture} className="img-fluid rounded" alt="Alyssa Reyes headshot" />
-          <a href="REYES-RESUME.pdf" download="REYES-RESUME" className="roboto-mono-ap m-2">RESUME</a>
-          <a href="https://github.com/2REYES1" className="roboto-mono-ap m-2 ">GITHUB</a>
-          
+
+
+      <div className="row g-0 h-100 align-items-center justify-content-center">
+
+        <div className="col-md-3 justify-content-center text-center p-0 m-0">
+          <h3 className="nanum-pen-ap nanum-headers-ap">ALYSSA REYES</h3>
+          <img src={AlyssaPicture} className="img-fluid rounded img-responsive" alt="Alyssa Reyes headshot" />
+          {/* <a href="REYES-RESUME.pdf" download="REYES-RESUME" className="roboto-mono-ap m-2">RESUME</a>
+          <a href="https://github.com/2REYES1" className="roboto-mono-ap m-2 ">GITHUB</a> */}
         </div>
+
         <div className="col-sm justify-content-center text-left">
-          <h3 className="roboto-mono-ap">INTERESTS</h3>
-          <p className="roboto-mono-ap">
+
+          <p className="roboto-mono-ap roboto-headers-ap">INTERESTS</p>
+          <p className="roboto-mono-ap roboto-body-text-ap">
             front-end web development, full-stack web development, ui/ux design, game development, game design, artificial intelligence.
           </p>
         </div>
+
         <div className="col-sm justify-content-center text-left">
-          <h3 className="roboto-mono-ap">ABOUT</h3>
-          <p className="roboto-mono-ap">
+          <h3 className="roboto-mono-ap roboto-headers-ap">ABOUT</h3>
+          <p className="roboto-mono-ap roboto-body-text-ap">
             21 | she/her | entp-a
             <br />
             CS student at UC Irvine. From the Bay Area. Loves chocolate.
           </p>
-          <h3 className="roboto-mono-ap">HOBBIES</h3>
-          <p className="roboto-mono-ap">
+          <h3 className="roboto-mono-ap roboto-headers-ap">HOBBIES</h3>
+          <p className="roboto-mono-ap roboto-body-text-ap">
             basketball, volleyball, photography, videography, video editing, valorant, call of duty, pokemon.
           </p>
         </div>
+
       </div>
     </div>
   );
---
+++ reyes2024/src/Components/ProjectCard/ProjectCard.js
@@ -0,0 +1,14 @@
+import './ProjectCard.css';
+
+function ProjectCard(props) {
+  return (
+    <div className="card m-3" style={{ width: '18rem' }}>
+      <div className="card-body">
+
+        
+      </div>
+    </div>
+  );
+}
+
+export default ProjectCard;
