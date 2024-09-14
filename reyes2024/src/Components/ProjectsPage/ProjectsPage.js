9aa0634d597f7c6fa57287c9ce1a2f3495763a07
Alyssa Reyes <alyssalareyes@gmail.com>
2024-09-13 18:55:25 -0700

    added a project showcase page but nothing on there. trying to fix spacing on alyssapage

--- reyes2024/src/Components/ProjectsPage/ProjectsPage.js
+++ reyes2024/src/Components/ProjectsPage/ProjectsPage.js
@@ -1,24 +1,27 @@
 import './ProjectsPage.css';
+import React from 'react';
+import ProjectCard from '../ProjectCard/ProjectCard';
 
-function ProjectsPage(){
+function ProjectsPage() {
+  return (
+    <div className='container main-wrapper-proj-page' id="projects-page">
+      <div className='row justify-content-center nanum-pen-proj-page'>
+        Project Showcase
+      </div>
 
-    return(
-        <div className='container main-wrapper-proj-page'>
+      <div className='row justify-content-center'>
 
-            <div className='row justify-content-center nanum-pen-proj-page'>
-                Project Showcase
-            </div>
-
-            <div className='row justify-content-center'>
-                <div className='col-lg-2'>
-                    Project 1
-                </div>
-                <div className='col-lg-2'>
-                    Project 1 information.
-                </div>
-            </div>
+        <div className='col-lg-3'>
+          <ProjectCard 
+            title="Project 1" 
+            summary="a quick summary of this very cool project"
+            infoList={["React", "JavaScript", "CSS", "Frontend Development"]} 
+          />
         </div>
-    );
+
+      </div>
+    </div>
+  );
 }
 
 export default ProjectsPage;
