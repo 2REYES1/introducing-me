2412c344c7ce999eccdcba5495089ebc13228dbc
Alyssa Reyes <alyssalareyes@gmail.com>
2024-09-16 01:30:50 -0700

    fixed spacing for alyssapage

--- reyes2024/src/Components/AlyssaPage/AlyssaPage.js
+++ reyes2024/src/Components/AlyssaPage/AlyssaPage.js
@@ -10,16 +10,17 @@ function AlyssaPage() {
     <div className="container main-wrapper-ap" id="alyssa-page">
 
 
-      <div className="row g-0 h-100 align-items-center justify-content-center">
-
-        <div className="col-md-3 justify-content-center text-center p-0 m-0">
+      <div className="row h-100 align-items-center justify-content-center">
+      
+      <div className="row align-items-center justify-content-center">
+        <div className="col-md-3 justify-content-center text-center p-0 m-3">
           <h3 className="nanum-pen-ap nanum-headers-ap">ALYSSA REYES</h3>
           <img src={AlyssaPicture} className="img-fluid rounded img-responsive" alt="Alyssa Reyes headshot" />
           {/* <a href="REYES-RESUME.pdf" download="REYES-RESUME" className="roboto-mono-ap m-2">RESUME</a>
           <a href="https://github.com/2REYES1" className="roboto-mono-ap m-2 ">GITHUB</a> */}
         </div>
 
-        <div className="col-sm justify-content-center text-left">
+        <div className="col-sm justify-content-center text-left m-1">
 
           <p className="roboto-mono-ap roboto-headers-ap">INTERESTS</p>
           <p className="roboto-mono-ap roboto-body-text-ap">
@@ -27,7 +28,7 @@ function AlyssaPage() {
           </p>
         </div>
 
-        <div className="col-sm justify-content-center text-left">
+        <div className="col-sm justify-content-center text-left m-1">
           <h3 className="roboto-mono-ap roboto-headers-ap">ABOUT</h3>
           <p className="roboto-mono-ap roboto-body-text-ap">
             21 | she/her | entp-a
@@ -39,7 +40,7 @@ function AlyssaPage() {
             basketball, volleyball, photography, videography, video editing, valorant, call of duty, pokemon.
           </p>
         </div>
-
+      </div>
       </div>
     </div>
   );
