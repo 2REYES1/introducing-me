1b6cbf07ea53cea169859034396c991eb396db9c
Alyssa Reyes <alyssalareyes@gmail.com>
2024-09-18 02:05:47 -0700

    completed css for AlyssaPage

--- reyes2024/src/Components/AlyssaPage/AlyssaPage.js
+++ reyes2024/src/Components/AlyssaPage/AlyssaPage.js
@@ -12,9 +12,9 @@ function AlyssaPage() {
 
       <div className="row h-100 align-items-center justify-content-center">
       
-      <div className="row align-items-center justify-content-center">
+      <div className="row align-items-center justify-content-center gy-0">
 
-        <div className="col-md-3 justify-content-center text-center p-0 m-3">
+        <div className="col-md-4 justify-content-center text-center p-2 m-0">
           <h3 className="nanum-pen-ap nanum-headers-ap">ALYSSA REYES</h3>
           <img src={AlyssaPicture} className="img-fluid rounded img-responsive" alt="Alyssa Reyes headshot" />
           {/* Social media icons */}
@@ -30,24 +30,30 @@ function AlyssaPage() {
           </div>
         </div>
 
-        <div className="col-sm justify-content-center text-left p-0 m-0">
-          <p className="roboto-mono-ap roboto-headers-ap m-0">INTERESTS</p>
-          <p className="roboto-mono-ap roboto-body-text-ap">
-            front-end web development, full-stack web development, ui/ux design, game development, game design, artificial intelligence.
-          </p>
+        <div className="col-sm-4 justify-content-center text-left p-0 m-0">
+          <div className='ap-box'>
+            <p className="roboto-mono-ap roboto-headers-ap m-0 p-0">INTERESTS</p>
+            <p className="roboto-mono-ap roboto-body-text-ap">
+              front-end web development, full-stack web development, ui/ux design, game development, game design, artificial intelligence.
+            </p>
+          </div>
         </div>
 
-        <div className="col-sm justify-content-center text-left p-0 m-0">
-          <h3 className="roboto-mono-ap roboto-headers-ap m-0">ABOUT</h3>
-          <p className="roboto-mono-ap roboto-body-text-ap">
-            21 | she/her | entp-a
-            <br />
-            CS student at UC Irvine. From the Bay Area. Loves chocolate.
-          </p>
-          <h3 className="roboto-mono-ap roboto-headers-ap m-0">HOBBIES</h3>
-          <p className="roboto-mono-ap roboto-body-text-ap">
-            basketball, volleyball, photography, videography, video editing, valorant, call of duty, pokemon.
-          </p>
+        <div className="col-sm-4 justify-content-center text-left p-0 m-0">
+          <div className='ap-box mb-3'>
+            <h3 className="roboto-mono-ap roboto-headers-ap m-0 p-0">ABOUT</h3>
+            <p className="roboto-mono-ap roboto-body-text-ap">
+              21 | she/her | entp-a
+              <br />
+              CS student at UC Irvine. From the Bay Area. Loves chocolate.
+            </p>
+          </div>
+          <div className='ap-box'>
+            <h3 className="roboto-mono-ap roboto-headers-ap p-0 m-0">HOBBIES</h3>
+            <p className="roboto-mono-ap roboto-body-text-ap">
+              basketball, volleyball, photography, videography, video editing, valorant, call of duty, pokemon.
+            </p>
+          </div>
         </div>
       </div>
       </div>
