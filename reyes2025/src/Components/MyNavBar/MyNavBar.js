c4ffd1090ac3348f50baaa0d05d168a0463dc1e0
Alyssa Reyes <alyssalareyes@gmail.com>
2025-07-10 01:59:08 -0700

    base css for greetingpage and navbar

--- reyes2025/src/Components/MyNavBar/MyNavBar.js
+++ reyes2025/src/Components/MyNavBar/MyNavBar.js
@@ -2,18 +2,18 @@ import './MyNavBar.css';
 
 function MyNavBar(props){
     return(
-        <nav className="fixed top-0 left-0 right-0 bg-gray-800 text-white py-4 shadow-lg">
-          <div className="container mx-auto flex justify-center items-center space-x-8">
-            <a className="font-mono text-lg uppercase tracking-wide hover:text-blue-300 tranistion duration-300 ease-in-out" href={props.greetingHref}>
-              Greeting
+        <nav className="fixed top-0 left-0 right-0 bg-black text-white py-2 shadow-lg">
+          <div className="container mx-auto flex justify-center items-center space-x-8 font-mono text-sm">
+            <a className="" href={props.greetingHref}>
+              GREETING
             </a>
 
-            <a className="font-mono text-lg uppercase tracking-wide hover:text-blue-300 tranistion duration-300 ease-in-out" href={props.greetingHref}>
-              Alyssa
+            <a className="" href={props.aboutHref}>
+              ALYSSA
             </a>
 
-            <a className="font-mono text-lg uppercase tracking-wide hover:text-blue-300 tranistion duration-300 ease-in-out" href={props.greetingHref}>
-              Projects
+            <a className="" href={props.projectsHref}>
+              PROJECTS
             </a>
           </div>
         </nav>
