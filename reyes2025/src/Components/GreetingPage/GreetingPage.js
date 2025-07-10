646e06da1ce75dd55ce4dc503ee995016e93802e
Alyssa Reyes <alyssalareyes@gmail.com>
2025-07-10 01:02:53 -0700

    del unneeded files. adjusted some greeting page

--- reyes2025/src/Components/GreetingPage/GreetingPage.js
+++ reyes2025/src/Components/GreetingPage/GreetingPage.js
@@ -6,11 +6,11 @@ function GreetingPage() {
   return (
     <div id="greeting-page" className="flex flex-col items-center justify-center min-h-screen p-4">
       <div className="text-center">
-        <p className="font-nanum-pen font-semibold text-black-4xl md:text-5xl lg-text-7xl mb-4">
-          Hello, I'm{' '}
-          <span className="font-nanum-pen relative inline-block">
-           Alyssa Reyes
-          </span>
+        <p className="font-nanum-pen font-semibold text-black-4xl md:text-5xl lg-text-7xl">
+          Hello, I'm
+        </p>
+        <p className="font-nanum-pen font-bold">
+          Alyssa Reyes
         </p>
         <p className="font-roboto-mono italic text-sm md:text-base lg:text-lg mb-4 md:mb-6 max-w-lg md:max-w-2xl mx-auto">
           a uc irvine computer science graduate who loves the endless knowledge that comes with tech
