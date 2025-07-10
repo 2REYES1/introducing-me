2575a810fbc5b62d63dab60be251f260a14e7d6d
Alyssa Reyes <alyssalareyes@gmail.com>
2025-07-10 03:22:44 -0700

    made alyssa page more responsive

--- reyes2025/src/Components/AlyssaPage/AlyssaPage.js
+++ reyes2025/src/Components/AlyssaPage/AlyssaPage.js
@@ -8,17 +8,21 @@ import AlyssaPicture from './imgs/ally.JPG';
 function AlyssaPage() {
   return (
     <div id="alyssa-page" className="min-h-screen flex flex-col items-center justify-center p-4">
-      <div className="flex flex-row">
-        <img src={AlyssaPicture} className="img-fluid img-responsive w-[300px]"/>
-        <div className="flex flex-col justify-center text-left p-4 font-mono">
-          <p className="text-7xl font-bold">ABOUT ME</p>
-          <p className="text-lg">uc irvine computer science co '25</p>
-          <div className="text-base">
-            <p className="font-bold italic">INTERESTS:</p>
-            <p>front-end web development, full-stack web development, machine learning, ui/ux design, game development, game design, data science</p>
-
-            <p className="font-bold italic">HOBBIES:</p>
-            <p>basketball, volleyball, photography, filmmaking, video editing, video games</p>
+      <div className="flex flex-col md:flex md:flex-row md:m-4 md:space-x-4 items-center justify-center">
+        <img src={AlyssaPicture} alt="Alyssa Reyes" className="object-cover w-[400px] h-[250px] sm:w-[400px] sm:h-[250px] md:w-[300px] md:h-[400px] lg:w-[500px] lg:h-[600px]"/>
+        <div className="flex flex-col justify-center text-center sm:text-center md:text-left p-4 font-mono">
+          <p className="text-4xl text-center font-bold sm:text-left lg:text-7xl">ABOUT ME</p>
+          <p className="text-md text-left italic">uc irvine computer science co '25</p>
+          <p className="text-md mb-[2rem] text-left italic">she/her</p>
+          <div className="text-md space-y-2">
+            <div>
+              <p className="font-bold text-left sm:text-left italic">INTERESTS:</p>
+              <p className="text-right lg:ml-[10rem]">front-end web development, full-stack web development, machine learning, ui/ux design, game development, game design, data science</p>              
+            </div>
+            <div>
+              <p className="font-bold text-left sm:text-left italic">HOBBIES:</p>
+              <p className="text-right lg:ml-[10rem]">basketball, volleyball, photography, filmmaking, video editing, video games</p>              
+            </div>
           </div>
         </div>
 
