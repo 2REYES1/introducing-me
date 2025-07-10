6faa97cdbbeb7268a01e9845db22a0ee10cde790
Alyssa Reyes <alyssalareyes@gmail.com>
2025-07-10 01:59:08 -0700

    base css for greetingpage and navbar

--- reyes2025/src/Components/GreetingPage/GreetingPage.js
+++ reyes2025/src/Components/GreetingPage/GreetingPage.js
@@ -5,17 +5,18 @@ import EmailMeButton from '../EmailMeButton/EmailMeButton';
 function GreetingPage() {
   return (
     <div id="greeting-page" className="flex flex-col items-center justify-center min-h-screen p-4">
-      <div className="text-center">
-        <p className="font-nanum-pen font-semibold text-black-4xl md:text-5xl lg-text-7xl">
-          Hello, I'm
+      <div className="text-left font-mono w-full">
+        <p className="text-base">
+          hello, i'm
         </p>
-        <p className="font-nanum-pen font-bold">
-          Alyssa Reyes
+      </div>
+      <div className="text-center font-mono">
+        <p className="text-7xl font-bold mb-2">
+          ALYSSA REYES
         </p>
-        <p className="font-roboto-mono italic text-sm md:text-base lg:text-lg mb-4 md:mb-6 max-w-lg md:max-w-2xl mx-auto">
+        <p className="text-base italic">
           a uc irvine computer science graduate who loves the endless knowledge that comes with tech
         </p>
-
       </div>
 
     </div>
