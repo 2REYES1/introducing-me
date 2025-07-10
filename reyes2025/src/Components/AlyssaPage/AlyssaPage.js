15492648cf1ec57a4203d0dd182fccd92dac061c
Alyssa Reyes <alyssalareyes@gmail.com>
2025-07-10 03:28:50 -0700

    made greeting text responsive

--- reyes2025/src/Components/AlyssaPage/AlyssaPage.js
+++ reyes2025/src/Components/AlyssaPage/AlyssaPage.js
@@ -12,15 +12,15 @@ function AlyssaPage() {
         <img src={AlyssaPicture} alt="Alyssa Reyes" className="object-cover w-[400px] h-[250px] sm:w-[400px] sm:h-[250px] md:w-[300px] md:h-[400px] lg:w-[500px] lg:h-[600px]"/>
         <div className="flex flex-col justify-center text-center sm:text-center md:text-left p-4 font-mono">
           <p className="text-4xl text-center font-bold sm:text-left lg:text-7xl">ABOUT ME</p>
-          <p className="text-md text-left italic">uc irvine computer science co '25</p>
-          <p className="text-md mb-[2rem] text-left italic">she/her</p>
-          <div className="text-md space-y-2">
+          <p className="text-sm text-left italic">uc irvine computer science co '25</p>
+          <p className="text-sm mb-[2rem] text-left italic">she/her</p>
+          <div className="text-sm space-y-2">
             <div>
-              <p className="font-bold text-left sm:text-left italic">INTERESTS:</p>
+              <p className="font-bold text-left sm:text-left italic md:text-lg lg:text-xl">INTERESTS:</p>
               <p className="text-right lg:ml-[10rem]">front-end web development, full-stack web development, machine learning, ui/ux design, game development, game design, data science</p>              
             </div>
             <div>
-              <p className="font-bold text-left sm:text-left italic">HOBBIES:</p>
+              <p className="font-bold text-left sm:text-left italic md:text-lg lg:text-xl">HOBBIES:</p>
               <p className="text-right lg:ml-[10rem]">basketball, volleyball, photography, filmmaking, video editing, video games</p>              
             </div>
           </div>
--- reyes2025/src/Components/GreetingPage/GreetingPage.js
+++ reyes2025/src/Components/GreetingPage/GreetingPage.js
@@ -6,15 +6,15 @@ function GreetingPage() {
   return (
     <div id="greeting-page" className="flex flex-col items-center justify-center min-h-screen p-4">
       <div className="text-left font-mono w-full">
-        <p className="text-base">
+        <p className="text-sm">
           hello, i'm
         </p>
       </div>
       <div className="text-center font-mono">
-        <p className="text-7xl font-bold mb-2">
+        <p className="text-5xl font-bold mb-2 md:text-7xl">
           ALYSSA REYES
         </p>
-        <p className="text-base italic">
+        <p className="text-sm italic">
           a uc irvine computer science graduate who loves the endless knowledge that comes with tech
         </p>
       </div>
