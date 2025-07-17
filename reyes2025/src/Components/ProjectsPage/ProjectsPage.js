ad2c735f1ed8372c6ba95af38ff138fa99318287
Alyssa Reyes <alyssalareyes@gmail.com>
2025-07-17 03:09:02 -0700

    able to get firebase setup

--- reyes2025/src/Components/ProjectsPage/ProjectsPage.js
+++ reyes2025/src/Components/ProjectsPage/ProjectsPage.js
@@ -1,29 +1,53 @@
 import './ProjectsPage.css';
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import ProjectCard from '../ProjectCard/ProjectCard';
+import {getProjects} from '../../firebase.js';
+
 
 function ProjectsPage() {
+  const [projects, setProjects] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  // const [newProjectTitle, setNewProjectTitle] = useState('');
+  // const [newProjectSponsor, setNewProjectSponsor] = useState('');
+  // const [newProjectDate, setNewProjectDate] = useState('');
+  // const [newProjectInfo, setNewProjectInfo] = useState(''); 
+  // const [newProjectLink, setNewProjectLink] = useState('');
+  // const [newProjectTools, setNewProjectTools] = useState('');
+
+  useEffect(() => {
+    const fetchAllProjects = async () => {
+      try {
+        setLoading(true);
+        setError(null);
+        const fetchedProjects = await getProjects();
+        setProjects(fetchedProjects);
+      } catch (err) {
+        console.error("Error fetching projects: ", err);
+        setError(err.message || "Failed to load projects.");
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchAllProjects();
+  }, [])
+
   return (
     <div id="projects-page" className="flex flex-col items-center justify-center min-h-screen p-4">
       <p className="text-4xl font-bold py-10">PROJECT SHOWCASE</p>
-      <ProjectCard
-        projectTitle="Project 1 superlong title number"
-        projectDate="jan 2025-present"
-        projectInfo={[
-          "Designed and implemented UI for game menus, HUDs, and in-game interfaces using C# and Figma, optimizing player engagement and experience",
-          "Conducted monthly user testing and gathered feedback to refine the user interface",
-          "Contributed to the successful launch of multiple games on itch.io",
-          "Conducted monthly user testing and gathered feedback to refine the user interface",
-          "Contributed to the successful launch of multiple games on itch.io",
-          "Conducted monthly user testing and gathered feedback to refine the user interface",
-          "Contributed to the successful launch of multiple games on itch.io",
-          "Conducted monthly user testing and gathered feedback to refine the user interface",
-          "Contributed to the successful launch of multiple games on itch.io"
-        ]}
-        projectLink="https://github.com/2REYES1"
-        projectTools={["firebase", "react", "aws", "supabse", "javascript", "figma", "vscode", "c#"]}
+      {projects.map((project) => (
+        <ProjectCard
+        key={project.id}
+        projectTitle={project.projectTitle}
+        projectSponsor={project.projectSponsor}
+        projectDate={project.projectDate}
+        projectInfo={project.projectInfo}
+        projectLink={project.projectLink}
+        projectTools={project.projectTools}
       >
       </ProjectCard>
+      ))}
      
            
     </div>
