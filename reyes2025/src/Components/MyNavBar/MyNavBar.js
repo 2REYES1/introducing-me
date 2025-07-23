a459dadad8233cc9c2b8e3829a1ae36b96abed9d
Alyssa <alyssalareyes@gmail.com>
2025-07-23 02:38:45 -0700

    added icons to nav bar

--- reyes2025/src/Components/MyNavBar/MyNavBar.js
+++ reyes2025/src/Components/MyNavBar/MyNavBar.js
@@ -1,18 +1,33 @@
 import './MyNavBar.css';
+import {Github, Linkedin, FileText} from 'lucide-react';
 
 function MyNavBar(props){
+    const{
+      githubLink ="https://github.com/2REYES1",
+      linkedInLink="https://www.linkedin.com/in/alyssalareyes/",
+      resumeLink="./Alyssa_Reyes-ResumeCS.pdf"
+    } = props;
+
     return(
         <nav className="fixed top-0 left-0 right-0 bg-black text-white py-2 shadow-lg">
-          <div className="container mx-auto flex justify-center items-center space-x-8 font-mono text-sm">
-            <a className="" href={props.greetingHref}>
-              GREETING
-            </a>
-
-            <a className="" href={props.aboutHref}>
+          <div className="container mx-auto flex flex-row justify-center items-center space-x-4 font-mono text-sm">
+            <div className="absolute flex flex-row justify-start w-full pl-4 space-x-2"> 
+              <a href={linkedInLink} target="_blank" rel="noopener noreferrerr" aria-label="Alyssa's LinkedIn">
+                <Linkedin size={20}/>
+              </a>
+              <a href={resumeLink} target="_blank" rel="noopener noreferrerr" aria-label="Alyssa's Resume">
+                <FileText size={20}/>
+              </a>
+              <a href={githubLink}target="_blank" rel="noopener noreferrerr" aria-label="Alyssa's Github">
+                <Github size={20}/>
+              </a>
+            </div>
+            
+            <a className="z-50" href={props.aboutHref}>
               ALYSSA
             </a>
 
-            <a className="" href={props.projectsHref}>
+            <a className="z-50" href={props.projectsHref}>
               PROJECTS
             </a>
           </div>
