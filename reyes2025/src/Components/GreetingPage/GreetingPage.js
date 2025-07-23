0b40eaf78f2d23f82b37c6bc839f13d57c1ac895
Alyssa <alyssalareyes@gmail.com>
2025-07-23 00:55:54 -0700

    added css to greeing bg

--- reyes2025/src/Components/GreetingPage/GreetingPage.js
+++ reyes2025/src/Components/GreetingPage/GreetingPage.js
@@ -4,21 +4,22 @@ import EmailMeButton from '../EmailMeButton/EmailMeButton';
 
 function GreetingPage() {
   return (
-    <div id="greeting-page" className="flex flex-col items-center justify-center min-h-screen p-4">
-      <div className="text-left font-mono w-full">
-        <p className="text-sm">
-          hello, i'm
-        </p>
+    <div id="greeting-page" className="flex flex-col w-full items-center justify-center min-h-screen bg-black text-white">
+      <div className="flex flex-col items-center justify-center p-4">
+        <div className="text-left font-mono w-full">
+          <p className="text-sm">
+            hello, i'm
+          </p>
+        </div>
+        <div className="text-center font-mono">
+          <p className="text-5xl font-bold mb-2 md:text-7xl text-[#C9E843]">
+            ALYSSA REYES
+          </p>
+          <p className="text-sm italic">
+            a uc irvine computer science graduate who loves the endless knowledge that comes with tech
+          </p>
+        </div> 
       </div>
-      <div className="text-center font-mono">
-        <p className="text-5xl font-bold mb-2 md:text-7xl">
-          ALYSSA REYES
-        </p>
-        <p className="text-sm italic">
-          a uc irvine computer science graduate who loves the endless knowledge that comes with tech
-        </p>
-      </div>
-
     </div>
   );
 }
