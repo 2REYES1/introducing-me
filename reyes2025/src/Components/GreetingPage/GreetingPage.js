d13b3e5c135febf6c7bb401ea315d90944429424
Alyssa <alyssalareyes@gmail.com>
2025-08-07 02:35:51 -0700

    greeting text now has typewriter effect

--- reyes2025/src/Components/GreetingPage/GreetingPage.js
+++ reyes2025/src/Components/GreetingPage/GreetingPage.js
@@ -3,18 +3,27 @@ import * as React from 'react';
 import EmailMeButton from '../EmailMeButton/EmailMeButton';
 import FadeInSection from '../FadeInSection/FadeInSection.js';
 import {motion, spring} from 'framer-motion';
+import {useTypewriter, Cursor} from 'react-simple-typewriter';
 
 function GreetingPage() {
+  const [text] = useTypewriter({
+    words: ['a uc irvine computer science graduate who loves the endless knowledge that comes with tech'],
+    loop: 1,
+    typespeed: 200
+    
+  })
   return (
     <div id="greeting-page" className="flex flex-col w-full items-center justify-center min-h-screen bg-black text-white ">
       <div className="flex flex-col items-center justify-center p-4">
+
         <div className="text-left w-full">
           <p className="text-lg md:text-2xl font-Inter">
             hello, i'm
           </p>
+        
         </div>
+
         <div className="text-center">
-          
           <motion.p drag dragSnapToOrigin
             initial={{ opacity: 0, scale: 1.2}}
             whileInView={{ opacity: 1, scale: 1}}
@@ -27,14 +36,16 @@ function GreetingPage() {
             }}
             className="text-7xl font-bold md:text-9xl lg:text-[170px] text-[#C9E843] font-Bebas">
             ALYSSA REYES
-          </motion.p>
-          
-          
-          
+          </motion.p>   
+
           <p className="text-sm md:text-base italic font-mono">
-            a uc irvine computer science graduate who loves the endless knowledge that comes with tech
+            {text}<Cursor/>
           </p>
+          
+            
+            
         </div> 
+
       </div>
     </div>
   );
