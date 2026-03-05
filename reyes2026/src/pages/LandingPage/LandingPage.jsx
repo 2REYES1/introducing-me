32ddc95dabc39560072d95ab22e999f3515729b2
Alyssa <alyssalareyes@gmail.com>
2026-03-05 01:25:49 -0800

    added header for main landing page

--- reyes2026/src/pages/LandingPage/LandingPage.jsx
+++ reyes2026/src/pages/LandingPage/LandingPage.jsx
@@ -9,7 +9,7 @@ function LandingPage() {
   };
 
   return (
-    <div className="min-h-screen bg-gray-900 text-white">
+    <div className="min-h-screen bg-gradient-to-b from-black to-[#B2D365] text-white">
 
       {/* Show popup if user hasn't entered */}
       {!entered && <WelcomeButton onEnter={handleEnter} />}
@@ -17,10 +17,18 @@ function LandingPage() {
       {/* Main landing page content */}
       {entered && (
         <div className="flex flex-col items-center justify-center h-screen">
-          <h1 className="text-5xl font-bold mb-4">Landing Page</h1>
-          <p className="text-xl text-gray-300">
-            This is the main content of the landing page.
-          </p>
+            <div className="w-[95%]">
+                <p className="font-['Orbit'] text-sm mb-1">LAST UPDATED: 3 March 2026</p>
+                <div className="text-white font-['Silkscreen'] text-4xl bg-black py-2 px-4">
+                    ALYRE.DEV
+                </div>
+                <div className="font-['Orbit'] text-center bg-[#6D6D6D] text-black shadow-md">
+                    <p className="drop-shadow-lg font-bold">
+                       <span>LINKEDIN</span> | <span>GITHUB</span> | <span>RESUME</span>  
+                    </p>
+                    
+                </div>
+            </div>
         </div>
       )}
 
