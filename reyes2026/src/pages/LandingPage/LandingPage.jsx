71b15e9524311662659ad9fa637a1c64a3020df8
Alyssa <alyssalareyes@gmail.com>
2026-03-07 03:53:18 -0800

    links and about me from database. added pro pic

--- reyes2026/src/pages/LandingPage/LandingPage.jsx
+++ reyes2026/src/pages/LandingPage/LandingPage.jsx
@@ -1,13 +1,41 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import WelcomeButton from "../../components/WelcomeButton/WelcomeButton";
 import ShowPanel from "../../components/ShowPanel/ShowPanel";
+import { getAboutMeInfo, getImportantLinks } from "../../services/firestore.js"
+import profilePic from "../../assets/me-picture.jpg";
 
 function LandingPage() {
   const [entered, setEntered] = useState(false);
+  const [aboutMeInfo, setAboutMeInfo] = useState("");
+  const [resumeLink, setResumeLink] = useState("");
+  const [githubLink, setGithubLink] = useState("");
+  const [linkedInLink, setLinkedInLink] = useState("");
 
   const handleEnter = () => {
     setEntered(true);
   };
+
+  useEffect(
+    () => {
+      const fetchAboutMe = async () => {
+        const [aboutMeInfoData, linksData] = await Promise.all([
+          getAboutMeInfo(),
+          getImportantLinks()
+        ]);
+
+
+
+        setAboutMeInfo(aboutMeInfoData.content);
+        setResumeLink(linksData.resumeLink);
+        setGithubLink(linksData.githubLink);
+        setLinkedInLink(linksData.linkedInLink);
+      };
+
+      fetchAboutMe()
+    },
+    []
+  )
+
 
   return (
     <div className="min-h-screen bg-gradient-to-b from-black to-[#B2D365] text-white">
@@ -23,40 +51,41 @@ function LandingPage() {
             {/* HEADER PORTION */}
             <div id="header-portion" className="w-[95%] mt-10">
                 <p className="font-['Orbit'] text-sm mb-1">LAST UPDATED: 3 March 2026</p>
-                <div className="text-white font-['Silkscreen'] text-4xl bg-black py-2 px-4 shadow-[0.375rem_0.375rem_0_black]">
-                    ALYRE.DEV
+                <div className="text-white font-['Silkscreen'] text-4xl bg-black py-2 shadow-[0.375rem_0.375rem_0_black]">
+                    ALYSSA REYES
                 </div>
                 <div className="font-['Orbit'] text-center bg-[#6D6D6D] text-black shadow-[0.375rem_0.375rem_0_black]">
                     <p className="drop-shadow-lg font-bold">
-                       <span>LINKEDIN</span> | <span>GITHUB</span> | <span>RESUME</span>  
+                       <span><a href={linkedInLink} target="_blank" rel="noopener noreferrer" className="hover:text-[#B2D365] hover:text-lg transition">
+                        LINKEDIN</a></span> | <span><a href={githubLink} target="_blank" rel="noopener noreferrer" className="hover:text-[#B2D365] hover:text-lg transition">GITHUB</a></span> | <span><a href={resumeLink} target="_blank" rel="noopener noreferrer" className="hover:text-[#B2D365] hover:text-lg transition">RESUME</a></span>  
                     </p>
                     
                 </div>
             </div>
 
             {/* ABOUT ME PORTION */}
-            <div className="mt-3 flex items-center justify-center w-full min-h-[15%] max-h-[15%] mb-12">
-              <div className="grid grid-cols-[1.5fr_3fr] w-[95%] h-full">
+            <div id="about-me-row" className="mt-3 flex items-center justify-center w-full mb-3 gap-4">
 
-                <div id="profile-pic-box" className="bg-gray-300 border-4 border-black z-10 relative w-[115%] h-[120%] shadow-[0.2rem_0.2rem_0_black]">
-                </div>
-
-                <div id="about-me-box" className="">
-                  <h1 className="font-['Silkscreen'] text-[#B2D365] flex items-center justify-center text-[2.5rem] relative z-50 translate-x-2 -translate-y-3 [text-shadow:.2rem_.2rem_0_black] [-webkit-text-stroke:.1rem_black] font-medium">
-                    ABOUT ME
-                  </h1>
-                  
-                  <div className="bg-gray-300 border-black relative -translate-y-10 h-[110%] flex items-center justify-center shadow-[0.375rem_0.375rem_0_black] border-4">
-                    <p className="font-['Orbit'] text-sm text-center text-black">
-                      Hello!
-                    </p>
-                  </div>
-                 
-                </div>
-                
-                
-
+              <div
+                id="profile-pic-box"
+                className="h-[200px] aspect-[3/4] bg-gray-300 border-4 border-black z-10 relative shadow-[0.2rem_0.2rem_0_black] overflow-hidden rounded-lg shrink-0 "
+              >
+                <img
+                  src={profilePic}
+                  alt="Profile"
+                  className="w-full h-full object-cover"
+                />
               </div>
+
+              <div className="flex-1 h-[200px] flex flex-col">
+                <p className="font-['Silkscreen'] z-20 text-4xl text-[#B2D365] [-webkit-text-stroke:2px_black] drop-shadow-[4px_4px_0_rgba(0,0,0,1)]">
+                  ABOUT ME
+                </p>
+                <div className="flex-1 overflow-y-auto font-['Orbit'] text-sm py-5 pl-10 pr-4 w-[120%] -translate-x-10 -translate-y-5 bg-gray-300 text-black font-black border-5 border-black shadow-[0.2rem_0.2rem_0_black]">
+                  {aboutMeInfo}
+                </div>
+              </div>
+
             </div>
 
             {/* SHOW PANEL PORTION */}
--- reyes2026/src/services/firestore.js
+++ reyes2026/src/services/firestore.js
@@ -1,5 +1,5 @@
 import { db } from "../firebase/firebase.js";
-import { collection, getDocs, query, orderBy } from "firebase/firestore";
+import { collection, getDocs, query, orderBy, doc, getDoc } from "firebase/firestore";
 
 function formatMonthDayYear(timestamp) {
   if (!timestamp) return "";
@@ -89,4 +89,30 @@ export async function getProjects(){
         };
     });
 
+}
+
+export async function getAboutMeInfo(){
+    const docRef = doc(db, "BasicInfo", "AboutMe");
+    const docSnap = await getDoc(docRef);
+
+    if (docSnap.exists()){
+        return docSnap.data();
+
+    } else {
+        console.log("AboutMe doc does not exist.");
+        return null
+    }
+}
+
+export async function getImportantLinks(){
+    const docRef = doc(db, "BasicInfo", "Links");
+    const docSnap = await getDoc(docRef);
+
+    if (docSnap.exists()){
+        return docSnap.data();
+
+    } else {
+        console.log("AboutMe doc does not exist.");
+        return null
+    }
 }
