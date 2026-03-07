866adff718e332a824b2f93e4054f32862d79327
Alyssa <alyssalareyes@gmail.com>
2026-03-07 01:41:21 -0800

    created project card

--- reyes2026/src/components/ProjectCard/ProjectCard.jsx
+++ reyes2026/src/components/ProjectCard/ProjectCard.jsx
@@ -1,39 +1,64 @@
 import { useState } from "react";
 import FolderIcon from "../FolderIcon/FolderIcon.jsx";
 
-function ProjectCard() {
+function ProjectCard(props) {
   const [open, setOpen] = useState(false);
 
   return (
     <div className="flex flex-col items-center">
 
       {/* Folder Icon */}
-      <FolderIcon onClick={() => setOpen(true)} />
+      <FolderIcon onClick={() => setOpen(true)} className="w-40 h-40 text-black hover:scale-110 transition drop-shadow-[4px_5px_0_rgba(0,0,0,0.25)]" />
+      <p className={"text-black font-['Orbit']"}>{props.projectName}</p>
 
       {/* Popup */}
       {open && (
         <div
-          className="z-20 fixed inset-0 flex items-center justify-center bg-black/40 text-black"
+          className="z-70 fixed inset-0 flex items-center justify-center bg-black/40 text-black"
           onClick={() => setOpen(false)}
         >
-          <div
-            className="bg-white border-2 border-black p-6 shadow-lg"
+          <div id="popup-box"
+            className="bg-gray-300 border-2 border-black p-4 shadow-lg rounded-lg flex flex-col items-center justify-center w-[95vw] max-w-xs"
             onClick={(e) => e.stopPropagation()}
           >
-            <h2 className="text-lg font-bold mb-2 text-black">
-              Project Info
-            </h2>
+            <div id="project-card-header" className="flex justify-between items-center w-full">
+              <p className="font-['Silkscreen'] text-3xl text-[#B2D365] [text-shadow:.2rem_.2rem_0_black] [-webkit-text-stroke:.1rem_black] font-medium">
+                {props.projectName}
+              </p>
 
-            <p>
-              This is a simple popup.
-            </p>
+              <button
+                onClick={() => setOpen(false)}
+                className="font-['Silkscreen'] bg-gray-300 border-2 border-black px-2 text-md hover:bg-gray-400 hover:scale-110 transition"
+              >
+                X
+              </button>
 
-            <button
-              onClick={() => setOpen(false)}
-              className="mt-4 border border-black px-3 py-1"
-            >
-              Close
-            </button>
+            
+            </div>
+
+
+            <div id="projects-content-section" className="bg-[#6D6D6D] border-2 border-black font-['Orbit'] max-h-[200px] overflow-y-auto text-sm shadow-[0.375rem_0.375rem_0_rgba(0,0,0,0.25)] my-2">
+                <p className="px-3 pt-1 text-left">
+                  {props.description?.map((item, index) => (
+                    <div key={index} className="py-1">∘ {item}</div>
+                  ))} 
+                </p>
+                <p className="px-3 pb-2 text-left" >
+                  <span className="text-black font-bold">Technologies: </span>
+                  {props.technologies?.map((item,index) => (
+                    <span key={index}>
+                      {item},{" "}   
+                    </span>
+                  ))}
+                </p>
+            
+            </div>
+
+
+            <div className="w-full flex justify-end pt-2">
+              <a className="bg-[#B2D365] py-1 px-2 text-xs border-2 border-black font-['Orbit'] shadow-[0.2rem_0.2rem_0_rgba(0,0,0,0.25)] hover:bg-gray-400 hover:scale-110 transition" href={props.githubLink}>VIEW PROJECT</a>
+            </div>
+
           </div>
         </div>
       )}
--- reyes2026/src/components/ProjectsPanel/ProjectsPanel.jsx
+++ reyes2026/src/components/ProjectsPanel/ProjectsPanel.jsx
@@ -2,17 +2,18 @@ import ProjectCard from "../ProjectCard/ProjectCard";
 
 function ProjectsPanel(){
 
-    const projects = [
-        { company: "Super Cool Company", role: "Software Engineer Intern", date: "jan 2026 - present", description: ["bullet1","bullet2","bullet3", "bullet2","bullet3", "bullet4"], tech: ["tech1","tech2","tech"] },
-        { company: "2", role: "Software Engineer Intern", date: "jan 2026 - present", description: ["bullet1","bullet2","bullet3"], tech: ["tech1","tech2","tech"] },
-        { company: "3", role: "Software Engineer Intern", date: "jan 2026 - present", description: ["bullet1","bullet2","bullet3", "bullet4","bullet2","bullet3", "bullet4"], tech: ["tech1","tech2","tech"] }
-];
+    const projects = [ 
+        {projectName: "golf-tracker", description: ["point1", "point2","point3", "point4", "point5","point3", "point4", "point5"], technologies: ["tech1", "tech2", "tech3", "tech4", "tech5"], githubLink: "https://2reyes1.github.io/introducing-me/", demo: "https://2reyes1.github.io/introducing-me/", date: "july 2027"},
+        {projectName: "golf-tracker", description: ["point1", "point2","point3", "point4", "point5","point3", "point4", "point5"], technologies: ["tech1", "tech2", "tech3", "tech4", "tech5"], githubLink: "https://2reyes1.github.io/introducing-me/", demo: "https://2reyes1.github.io/introducing-me/", date: "july 2027"},
+        {projectName: "golf-tracker", description: ["point1", "point2","point3", "point4", "point5","point3", "point4", "point5"], technologies: ["tech1", "tech2", "tech3", "tech4", "tech5"], githubLink: "https://2reyes1.github.io/introducing-me/", demo: "https://2reyes1.github.io/introducing-me/", date: "july 2027"}
+        
+    ];
 
     return(
         <div className="flex flex-col items-center">
             {projects.map(projects => (
-                <div key={projects.company} className="flex flex-col items-center w-full">
-                    <ProjectCard/>
+                <div key={projects.projectName} className="flex flex-col items-center w-full">
+                    <ProjectCard projectName={projects.projectName} description={projects.description} technologies={projects.technologies} githubLink={projects.githubLink} demo={projects.demo}/>
                 </div>
             ))}
         </div>
