8250cc38b05bc98d5767577651b56eda03ccbfcd
Alyssa <alyssalareyes@gmail.com>
2026-03-07 18:23:09 -0800

    adjusted css of welcome button

--- reyes2026/src/components/WelcomeButton/WelcomeButton.jsx
+++ reyes2026/src/components/WelcomeButton/WelcomeButton.jsx
@@ -1,12 +1,12 @@
 function WelcomeButton({ onEnter }) {
   return (
     <div className="fixed inset-0 flex items-center justify-center bg-gradient-to-b from-black to-[#B2D365]">
-      <div className="bg-black p-10 shadow-lg font-['Orbit'] text-center m-10 rounded-sm">
-        <h1 className="text-xl font-bold mb-6 text-white">EXECUTE alyssa_reyes.exe?</h1>
+      <div className="bg-gray-300 border-4  border-black p-10 shadow-lg font-['Orbit'] text-center m-10 rounded-xl">
+        <h1 className="text-xl font-bold mb-6 text-black">EXECUTE alyssa_reyes.exe?</h1>
 
         <button
           onClick={onEnter}
-          className="px-6 py-2 bg-[#D9D9D9] rounded-lg hover:bg-blue-600 transition text-black"
+          className="font-['Silkscreen'] bg-[#B2D365] border-2 border-black px-2 text-md hover:bg-gray-400 hover:scale-110 transition rounded-lg px-4 py-2"
         >
           YES
         </button>
